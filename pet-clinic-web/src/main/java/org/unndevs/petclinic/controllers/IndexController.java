12c7e7b6c57b451e98fcabe9250594a887c1ff5a
Linda Ngwenya <Linda.Ngwenya@absa.co.za>
2019-05-27 19:01:14 +0200

    add index template and index controller. closes #7

---
+++ pet-clinic-web/src/main/java/org/unndevs/petclinic/controllers/IndexController.java
@@ -0,0 +1,12 @@
+package org.unndevs.petclinic.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+    @RequestMapping({"", "/", "index", "index.html"})
+    public String indexPage() {
+        return "index";
+    }
+}
