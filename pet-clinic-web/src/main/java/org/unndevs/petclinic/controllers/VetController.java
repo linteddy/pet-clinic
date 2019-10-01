48ac3203cfff9b10e9d73723869424f8c09452ad
Linda Ngwenya (ZA) <Linda.Ngwenya@absa.africa>
2019-10-01 02:39:46 +0200

    list all vets on vets index page. Closes #14.

--- pet-clinic-web/src/main/java/org/unndevs/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/org/unndevs/petclinic/controllers/VetController.java
@@ -1,12 +1,21 @@
 package org.unndevs.petclinic.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.unndevs.petclinic.services.VetService;
 
 @Controller
 public class VetController {
+    private final VetService vetService;
+
+    public VetController(final VetService vetService) {
+        this.vetService = vetService;
+    }
+
     @RequestMapping({"/vets", "/vets/index", "/vets/index.html"})
-    public String listVets() {
+    public String listVets(final Model model) {
+        model.addAttribute("vets",vetService.findAll());
         return "vets/index";
     }
 }
